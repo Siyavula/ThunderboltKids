50961107bb9d03b6540224900db120d9bbcb62b6
Ewald Zietsman <ewald.zietsman@gmail.com>
2013-08-25 17:14:19 +0200

    Replaced absolute links with relative links, site-wide, fixed download links, footers still need fixing

--- site-fixes.sh
+++ site-fixes.sh
@@ -8,21 +8,25 @@
 #
 # replace 'src="/' with 'src="'
 # ditto for href
-#find . -name "teachers-parents.html" -print | xargs sed -i 's/src=\"\//src=\"/g'
-#find . -name "teachers-parents.html" -print | xargs sed -i 's/href=\"\//href=\"/g'
+find . -name "teachers-parents.html" -print | xargs sed -i 's/src=\"\//src=\"/g'
+find . -name "teachers-parents.html" -print | xargs sed -i 's/href=\"\//href=\"/g'
 
 
+##
+## Replace 'http://thunder.beresponsive.net/' with ''
+## in the root folder
+#find . -maxdepth 1 -name "*.html" -print | xargs sed -i 's/href=\"http:\/\/thunder\.beresponsive\.net\//href=\"/g'
+#find Grade* -name "*.html" -print | xargs sed -i 's/href=\"http:\/\/thunder\.beresponsive\.net\//href=\"\.\.\/\.\.\//g'
 #
-# Replace 'http://thunder.beresponsive.net/' with ''
+## fix paths in other links 
+##
+## replace 'href="/' with 'href="../../'
+##
+## in the root folder, change links that point to '/blah' to 'blah'
+#find . -maxdepth 1 -name "*.html" -print | xargs sed -i 's/href=\"\//href=\"/g'
 #
-find . -name "*.html" -print | xargs sed -i 's/href=\"http:\/\/thunder\.beresponsive\.net\//href=\"/g'
-
-# fix paths in other links 
-#
-# replace 'href="/' with 'href="/ThunderboltKids'
-#
-# in the root folder, change links that point to '/blah' to 'blah'
-#find . -name "*.html" -print | xargs sed -i 's/href=\"\//href=\"\/ThunderboltKids\//g'
+## for the contentpages change to relative 'href="../../' instead of 'href="/'
+#find Grade* -name "*.html" -print | xargs sed -i 's/href=\"\//href=\"\.\.\/\.\.\//g'
 
 
 
